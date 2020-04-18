8326ef51a574f13d88f730bc3178e3528c489260
Joshua Watson <josh.m.watson@gmail.com>
2020-04-18 17:48:15 -0500

    Added Paralax and diagonal scrolling backgrounds

---
+++ Assets/Scripts/BgScroll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgScroll : MonoBehaviour
+{
+    public float bgSpeed;
+    public Renderer bgRend;
+
+    private void FixedUpdate()
+    {
+        bgRend.material.mainTextureOffset += new Vector2(bgSpeed * Time.fixedDeltaTime, 0f);
+    }
+}
