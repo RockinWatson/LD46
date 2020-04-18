3d7d7add7d498353274cfb1800bf6ca394e73faf
Joshua Watson <josh.m.watson@gmail.com>
2020-04-17 23:59:59 -0500

    Added Camera Shake

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public ShakeBehavior shakeBehavior;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            shakeBehavior.TriggerShake();
+        }
+    }
+}
---
+++ Assets/Scripts/ShakeBehavior.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class ShakeBehavior : MonoBehaviour
+{
+    // Transform of the GameObject you want to shake
+    private new Transform transform;
+
+    // Desired duration of the shake effect
+    private float shakeDuration = 0f;
+
+    [SerializeField]
+    private float shakeTime = 0.3f;
+
+    // A measure of magnitude for the shake. Tweak based on your preference
+    [SerializeField]
+    private float shakeMagnitude = 0.2f;
+
+    // A measure of how quickly the shake effect should evaporate
+    [SerializeField]
+    private float dampingSpeed = 2.0f;
+
+    // The initial position of the GameObject
+    Vector3 initialPosition;
+
+    void Awake()
+    {
+        if (transform == null)
+        {
+            transform = GetComponent<Transform>();
+        }
+    }
+
+    void OnEnable()
+    {
+        initialPosition = transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (shakeDuration > 0)
+        {
+            transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+
+            shakeDuration -= Time.deltaTime * dampingSpeed;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            transform.localPosition = initialPosition;
+        }
+    }
+
+    public void TriggerShake()
+    {
+        shakeDuration = shakeTime;
+    }
+}
