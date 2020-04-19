088c398eb1816d8fab1fe2ee9cef968f60256756
Greg Raab <halfskye@gmail.com>
2020-04-19 01:49:23 -0700

    Scripts: Add basic Scrap script

---
+++ Assets/Scripts/Scrap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrap : MonoBehaviour
+{
+    [SerializeField] private float _scrapAmount = 9f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            collision.GetComponent<PlayerBehavior>().AddScrap(_scrapAmount);
+
+            //Destroy(this.gameObject);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
