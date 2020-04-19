395432d518f746105bc7c59221b052c34ab14b8b
Joshua Watson <josh.m.watson@gmail.com>
2020-04-19 15:55:37 -0500

    Adjusted boxcolliders to enemies and Wave Times

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject _target;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rangeToStop;
+
+    private bool _isAtTarget = false;
+    private bool _isCollidingWithEnemy = false;
+
+    private void Update()
+    {
+        IsAtTarget();
+    }
+
+    private void IsAtTarget()
+    {
+        var distanceToTarget = Vector3.Distance(_target.transform.position, transform.position);
+        if (distanceToTarget <= _rangeToStop ||
+           (distanceToTarget <= _rangeToStop && _isCollidingWithEnemy))
+        {
+            _isAtTarget = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        GoToTarget();      
+    }
+
+    private void GoToTarget()
+    {
+        if (_isAtTarget == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, _speed * Time.fixedDeltaTime);
+        } 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "BadCat1")
+        {
+            _isCollidingWithEnemy = true;
+        }
+    }
+}
