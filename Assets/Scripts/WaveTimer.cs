72b6e4f573c18bd72942c2b588df45b1b89e1777
Joshua Watson <josh.m.watson@gmail.com>
2020-04-19 12:48:44 -0500

    Added Basic WaveSpawners and WaveTimer

---
+++ Assets/Scripts/WaveTimer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class WaveTimer : MonoBehaviour
+{
+    [SerializeField]private float _timer;
+
+    [SerializeField] private GameObject[] _waves;
+    [SerializeField] private float[] _waveTimers;
+
+    private void Start()
+    {
+        foreach (var wave in _waves)
+        {
+            wave.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        WaveActivations();
+    }
+
+    private void WaveActivations()
+    {
+        if (_timer >= _waveTimers[0])
+        {
+            _waves[0].SetActive(true);
+        }
+        if (_timer >= _waveTimers[1])
+        {
+            _waves[0].SetActive(false);
+            //TODO: Display Wave Num Text
+            _waves[1].SetActive(true);
+        }
+    }
+}
