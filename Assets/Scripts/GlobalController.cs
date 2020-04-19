698950a52a8d69e777eb7a004c0bb7d5a5575e2e
Joshua Watson <josh.m.watson@gmail.com>
2020-04-19 16:43:31 -0500

    Added GlobalController and EndGame Scene

---
+++ Assets/Scripts/GlobalController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GlobalController : MonoBehaviour
+    {
+        public static GlobalController Instance;
+
+        public int EnemiesKilled;
+        public int ScrapCollected;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                DontDestroyOnLoad(gameObject);
+                Instance = this;
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
