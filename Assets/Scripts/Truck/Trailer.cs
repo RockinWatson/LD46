e3dbb3852c4dbedd524a374cc5409ccc13885e37
Greg Raab <halfskye@gmail.com>
2020-04-20 01:28:35 -0700

    Scripts: Add Trailer script

---
+++ Assets/Scripts/Truck/Trailer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trailer : Attachment
+{
+    private List<AttachmentSocket> _armorSockets = new List<AttachmentSocket>();
+    public List<AttachmentSocket> GetArmorSockets() { return _armorSockets; }
+
+    private List<AttachmentSocket> _turretSockets = new List<AttachmentSocket>();
+    public List<AttachmentSocket> GetTurretSockets() { return _turretSockets; }
+
+    private void Awake()
+    {
+        AttachmentSocket[] sockets = this.GetComponentsInChildren<AttachmentSocket>();
+        foreach (var socket in sockets)
+        {
+            switch (socket.GetSocketType())
+            {
+                case AttachmentSocket.SocketType.Armor:
+                    {
+                        _armorSockets.Add(socket);
+                    }
+                    break;
+                case AttachmentSocket.SocketType.Turret:
+                    {
+                        _turretSockets.Add(socket);
+                    }
+                    break;
+                default:
+                    Debug.LogWarning("Unrecognized Socket Type");
+                    break;
+            }
+        }
+    }
+}
