7c06b80e0af7b0af0d1039350409af5da2c4f1e1
Joshua Watson <josh.m.watson@gmail.com>
2020-04-20 17:02:59 -0500

    Working On WaveTimer

--- Assets/Scripts/WaveTimer.cs
+++ Assets/Scripts/WaveTimer.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections;
+using System.Threading;
+using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,12 +12,10 @@ public class WaveTimer : MonoBehaviour
     [SerializeField] private GameObject[] _waves;
     [SerializeField] private float[] _waveTimers;
 
-    private Text _text;
+    [SerializeField]private Text _text = null;
 
     private void Start()
     {
-        _text = GetComponent<Text>();
-
         foreach (var wave in _waves)
         {
             wave.SetActive(false);
@@ -32,19 +32,26 @@ public class WaveTimer : MonoBehaviour
     {
         if (_timer >= _waveTimers[0])
         {
+            StartWaveText("One");
             _waves[0].SetActive(true);
         }
         if (_timer >= _waveTimers[1])
         {
+            StartWaveText("Two");
             _waves[0].SetActive(false);
             //TODO: Display Wave Num Text
             _waves[1].SetActive(true);
         }
     }
 
-    private GameObject[] GetEnemiesOnCurrentWave()
+    private void StartWaveText(string waveInt)
     {
-        var enemObjects = GameObject.FindGameObjectsWithTag("Enemy");
+        _text.text = "Wave " + waveInt;
+        StartCoroutine(FadeTextToFullAlpha(1f, _text));
+        StartCoroutine(Wait(2));
+        StopCoroutine("FadeTextToFullAlpha");
+        StartCoroutine(FadeTextToZeroAlpha(0f, _text));
+        StopCoroutine("FadeTextToZeroAlpha");
     }
 
     public IEnumerator FadeTextToFullAlpha(float t, Text i)
@@ -65,5 +72,10 @@ public class WaveTimer : MonoBehaviour
             i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
             yield return null;
         }
+    }
+
+    public IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
     }
 }
