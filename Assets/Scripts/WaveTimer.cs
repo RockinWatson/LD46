a0148e2ba12a1eab5f8ba163c91805c921fd1cb1
Joshua Watson <josh.m.watson@gmail.com>
2020-04-20 16:10:26 -0500

    Added Terrain Clouds, Font

--- Assets/Scripts/WaveTimer.cs
+++ Assets/Scripts/WaveTimer.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WaveTimer : MonoBehaviour
 {
@@ -8,8 +10,12 @@ public class WaveTimer : MonoBehaviour
     [SerializeField] private GameObject[] _waves;
     [SerializeField] private float[] _waveTimers;
 
+    private Text _text;
+
     private void Start()
     {
+        _text = GetComponent<Text>();
+
         foreach (var wave in _waves)
         {
             wave.SetActive(false);
@@ -33,6 +39,31 @@ public class WaveTimer : MonoBehaviour
             _waves[0].SetActive(false);
             //TODO: Display Wave Num Text
             _waves[1].SetActive(true);
+        }
+    }
+
+    private GameObject[] GetEnemiesOnCurrentWave()
+    {
+        var enemObjects = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    public IEnumerator FadeTextToFullAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
         }
     }
 }
